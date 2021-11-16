72ed300692ea94654358f8f554b38a97a85e9d17
Eduardo Pedroso <drdpedroso@gmail.com>
2021-11-16 15:51:19 -0300

    Expose OPCR schema (#8)
    * Expose opcr schema
    
    * 1.4.0

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 import { components as authorizerComponent } from './generated/authorizer'
 import { components as tenantComponent } from './generated/tenant'
+import { components as opcrComponent } from './generated/opcr'
 
-export type Definitions = authorizerComponent['schemas'] & tenantComponent['schemas']
+export type Definitions = authorizerComponent['schemas'] & tenantComponent['schemas'] & opcrComponent['schemas']
