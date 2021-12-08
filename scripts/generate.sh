a965a05fa6d69f652fb3d0959431819acae24ba6
authereal <88290408+authereal@users.noreply.github.com>
2021-12-08 17:10:54 -0500

    Add decision_logs service (#22)
    * Add decision_logs service
    
    * reorganize
    
    * fix
    
    * bump version
    
    * generate decision_logs types
    
    * Add CLI flag
    
    * revert commit hash
    
    * pr feedback
    
    * rerun generate after running `yarn update`
    
    Co-authored-by: authereal <authereal@users.noreply.github.com>

---
+++ scripts/generate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+source downloaded/commit_hash.sh
+
+if [[ -n "${FAIL}" ]]; then
+    exit $FAIL
+fi
+
+if [[ $1 == "--fetch-specs" ]]; then
+    for REQUIRED_ENV in "USERNAME" "READ_WRITE_TOKEN" "COMMIT_HASH"
+    do
+        if [[ -z "${!REQUIRED_ENV}" ]]; then
+        echo "$REQUIRED_ENV must be set"
+        FAIL=1
+        fi
+    done
+
+    echo "Removing existing specs in downloaded/specs/\n"
+    rm ./downloaded/specs/*
+fi
+
+for SERVICE in "tenant" "authorizer" "registry" "decision_logs"
+do
+    OUTPUT_PATH=./downloaded/specs/${SERVICE}.openapi.json
+
+    if [[ $1 == "--fetch-specs" ]]; then
+        TARGET=https://api.github.com/repos/aserto-dev/openapi-grpc/contents/publish/${SERVICE}/openapi.json?ref=${COMMIT_HASH}
+        echo "CURLing $TARGET to $OUTPUT_PATH"
+        curl \
+            --create-dirs -o "${OUTPUT_PATH}" -u "${USERNAME}:${READ_WRITE_TOKEN}" \
+            -H "Accept: application/vnd.github.v3.raw" \
+            "${TARGET}"
+    fi
+    
+    yarn openapi-typescript ${OUTPUT_PATH} --output ./generated/${SERVICE}.ts
+done
